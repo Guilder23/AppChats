2946f5c6a649af1c1936f3818fbda6070b36d4ba
GUILDER <pplg39395@gmail.com>
2026-09-08 09:33:03 -0400

    Corregir inicializacion ASGI en Render

--- config/asgi.py
+++ config/asgi.py
@@ -14,9 +14,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from apps.chat.routing import websocket_urlpatterns
 
 django_asgi_application = get_asgi_application()
+
+from apps.chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
 	'http': django_asgi_application,
